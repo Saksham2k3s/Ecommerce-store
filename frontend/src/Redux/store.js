616daf0162e485be584ba4f7387a38e21b4b0773
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2024-03-06 13:41:03 +0100

    adding token expiration

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import fetchProductReducer from './slice/fetchProduct';
-import productDetailsReducer from './slice/productDetails';
-import userLoginReducer from './slice/login'
-import userSignUpReducer from './slice/signUp'
-import checkAuthReducer from './slice/userAuth'
+import { configureStore } from "@reduxjs/toolkit";
+import fetchProductReducer from "./slice/fetchProduct";
+import productDetailsReducer from "./slice/productDetails";
+import userLoginReducer from "./slice/login";
+import userSignUpReducer from "./slice/signUp";
+import checkAuthReducer from "./slice/userAuth";
 export const store = configureStore({
-    reducer : {
-          fetchProduct : fetchProductReducer,
-          productDetails : productDetailsReducer,
-          userLogin : userLoginReducer,
-         userSignUp : userSignUpReducer,
-         checkAuth : checkAuthReducer
-    }
-})
+  reducer: {
+    fetchProduct: fetchProductReducer,
+    productDetails: productDetailsReducer,
+    userLogin: userLoginReducer,
+    userSignUp: userSignUpReducer,
+    checkAuth: checkAuthReducer,
+  },
+});
